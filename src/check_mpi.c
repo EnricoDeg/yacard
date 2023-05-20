c59a05d6e21165f7da83cd22a1dd4f54e82a3f5f
Enrico <k202136@dkrz.de>
2023-05-20 19:43:02 +0200

    avoid macros in MPI check function

--- src/check_mpi.c
+++ src/check_mpi.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "check_mpi.h"
 
-void check_mpi_error(int error_code, MPI_Comm comm) {
-  int rank;
-  MPI_Comm_rank(comm, &rank);
+void safe_mpi(int error_code) {
+    int rank;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    char error_string[MPI_MAX_ERROR_STRING];
+    int length_of_error_string, error_class;
 
-  char error_string[MPI_MAX_ERROR_STRING];
-  int length_of_error_string, error_class;
+    if (error_code != MPI_SUCCESS) {
+        MPI_Comm_rank(comm, &rank);
+        MPI_Error_class(error_code, &error_class);
+        MPI_Error_string(error_class, error_string, &length_of_error_string);
+        fprintf(stderr, "%3d: %s\n", rank, error_string);
+        MPI_Error_string(error_code, error_string, &length_of_error_string);
+        fprintf(stderr, "%3d: %s\n", rank, error_string);
+        MPI_Abort(comm, error_code);
+    }
 
-  MPI_Error_class(error_code, &error_class);
-  MPI_Error_string(error_class, error_string, &length_of_error_string);
-  fprintf(stderr, "%3d: %s\n", rank, error_string);
-  MPI_Error_string(error_code, error_string, &length_of_error_string);
-  fprintf(stderr, "%3d: %s\n", rank, error_string);
-  MPI_Abort(comm, error_code);
 }
--- src/check_mpi.h
+++ src/check_mpi.h
@@ -1,10 +1,3 @@
 #include <mpi.h>
 
-#define check_mpi_call(call, comm)              \
-  do {                                          \
-    int error_code = (call);                    \
-    if (error_code != MPI_SUCCESS)              \
-      check_mpi_error(error_code, comm);        \
-  } while(0)
-
-void check_mpi_error(int error_code, MPI_Comm comm);
+void safe_mpi(int error_code);
