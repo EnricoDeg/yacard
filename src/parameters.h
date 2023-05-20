6683a406ec6f3b03f46ff4b7d8f3d0ff997b384a
Enrico <k202136@dkrz.de>
2023-05-20 19:43:56 +0200

    extend parameters to call numerics interface

--- src/parameters.h
+++ src/parameters.h
@@ -3,6 +3,18 @@
 #define NRONE 1
 #define N45NO 0
 #define N45go 1
+#define MAX_MPI_REQUESTS 24
+#define BEFORE_DERIV 0
+#define BEFORE_FILTE 1
+#define EXCH_ALL 0
+#define EXCH_ONE 1
+
+// State variables
+#define SV_DENSITY 0
+#define SV_MOMENTUM_X 1
+#define SV_MOMENTUM_Y 2
+#define SV_MOMENTUM_Z 3
+#define SV_ENERGY 4
 
 // Boundary contidions
 #define BC_NON_REFLECTIVE   10
