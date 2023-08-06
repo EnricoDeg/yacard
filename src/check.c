e314cd789d7b4fb2a2e8024109452d46c43b5cfa
Enrico <k202136@dkrz.de>
2023-08-06 17:04:58 +0200

    extend check module for files opening and closing

--- src/check.c
+++ src/check.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
 #include "check.h"
 
 void *safe_malloc(int size) {
     void *ptr = malloc(size);
 
     if (!ptr && (size > 0)) {
-      fprintf(stderr, "malloc failed!");
+      fprintf(stderr, "malloc failed!\n");
       exit(EXIT_FAILURE);
     }
 
@@ -15,8 +17,34 @@ void *safe_malloc(int size) {
 
 void safe_free (void * ptr)
 {
-    if (ptr != NULL)
+    if (ptr != NULL) {
         free (ptr);
+    } else {
+    	int myid;
+        safe_mpi( MPI_Comm_rank(MPI_COMM_WORLD, &myid) );
+        fprintf(stderr, "%d: null pointer to free!\n", myid);
+        exit(EXIT_FAILURE);
+    }
+}
+
+FILE *safe_open(const char *filename, const char *mode) {
+    FILE *fp = fopen(filename, mode);
+    if (!fp) {
+        printf("Cannot open file: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    return fp;
+}
+
+void safe_close(FILE *fp) {
+    if (!fp) {
+        fprintf(stderr, "Error closing file: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    // /fclose(fp);
+    if(fclose(fp) != 0) {
+        fprintf(stderr, "Error closing file: %s", strerror(errno));
+    }
 }
 
 void safe_mpi(int error_code) {
--- src/check.h
+++ src/check.h
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include <mpi.h>
 
 void safe_mpi(int error_code);
 void *safe_malloc(int size);
 void safe_free (void * ptr);
 void finish(char * component, char * function);
+FILE *safe_open(const char *filename, const char *mode);
+void safe_close(FILE *fp);
